ea21cdd3ea1e42ba31f13c8c988cc587df0e64de
Daniel J. Pritchett <dpritchett@gmail.com>
2013-07-29 15:05:47 -0500

    Yanked out a bunch of stuff, figuring out what I need to put back in

--- app/models/spree/inventory_unit_decorator.rb
+++ app/models/spree/inventory_unit_decorator.rb
@@ -1,5 +1,5 @@
-module Spree
-  InventoryUnit.class_eval do
-    belongs_to :line_item, class_name: "Spree::LineItem"
-  end
-end
+#module Spree
+  #InventoryUnit.class_eval do
+    #belongs_to :line_item, class_name: "Spree::LineItem"
+  #end
+#end
--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -1,28 +1,28 @@
-module Spree
-  LineItem.class_eval do
-    scope :assemblies, -> { joins(:product => :parts).uniq }
+#module Spree
+  #LineItem.class_eval do
+    #scope :assemblies, -> { joins(:product => :parts).uniq }
 
-    def any_units_shipped?
-      OrderInventoryAssembly.new(self).inventory_units.any? do |unit|
-        unit.shipped?
-      end
-    end
+    #def any_units_shipped?
+      #OrderInventoryAssembly.new(self).inventory_units.any? do |unit|
+        #unit.shipped?
+      #end
+    #end
 
-    # Destroy and verify inventory so that units are restocked back to the
-    # stock location
-    def destroy_along_with_units
-      self.quantity = 0
-      OrderInventoryAssembly.new(self).verify
-      self.destroy
-    end
+    ## Destroy and verify inventory so that units are restocked back to the
+    ## stock location
+    #def destroy_along_with_units
+      #self.quantity = 0
+      #OrderInventoryAssembly.new(self).verify
+      #self.destroy
+    #end
 
-    private
-      def update_inventory
-        if self.product.assembly? && order.completed?
-          OrderInventoryAssembly.new(self).verify(target_shipment)
-        else
-          OrderInventory.new(self.order).verify(self, target_shipment)
-        end
-      end
-  end
-end
+    #private
+      #def update_inventory
+        #if self.product.assembly? && order.completed?
+          #OrderInventoryAssembly.new(self).verify(target_shipment)
+        #else
+          #OrderInventory.new(self.order).verify(self, target_shipment)
+        #end
+      #end
+  #end
+#end
