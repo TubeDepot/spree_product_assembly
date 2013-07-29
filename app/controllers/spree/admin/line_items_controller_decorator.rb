d89a28b14c428d3d1fec3523f63ea27a543b410d
Daniel J. Pritchett <dpritchett@gmail.com>
2013-07-29 16:20:10 -0500

    Stripped out tons of stuff

--- app/controllers/spree/admin/line_items_controller_decorator.rb
+++ app/controllers/spree/admin/line_items_controller_decorator.rb
@@ -1,15 +1,15 @@
-module Spree
-  module Admin
-    LineItemsController.class_eval do
-      def update
-        @line_item.update_attributes(params[:line_item])
-        render nothing: true
-      end
+#module Spree
+  #module Admin
+    #LineItemsController.class_eval do
+      #def update
+        #@line_item.update_attributes(params[:line_item])
+        #render nothing: true
+      #end
 
-      def destroy
-        @line_item.destroy_along_with_units
-        render nothing: true
-      end
-    end
-  end
-end
+      #def destroy
+        #@line_item.destroy_along_with_units
+        #render nothing: true
+      #end
+    #end
+  #end
+#end
--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -1,28 +1,5 @@
-#module Spree
-  #LineItem.class_eval do
-    #scope :assemblies, -> { joins(:product => :parts).uniq }
-
-    #def any_units_shipped?
-      #OrderInventoryAssembly.new(self).inventory_units.any? do |unit|
-        #unit.shipped?
-      #end
-    #end
-
-    ## Destroy and verify inventory so that units are restocked back to the
-    ## stock location
-    #def destroy_along_with_units
-      #self.quantity = 0
-      #OrderInventoryAssembly.new(self).verify
-      #self.destroy
-    #end
-
-    #private
-      #def update_inventory
-        #if self.product.assembly? && order.completed?
-          #OrderInventoryAssembly.new(self).verify(target_shipment)
-        #else
-          #OrderInventory.new(self.order).verify(self, target_shipment)
-        #end
-      #end
-  #end
-#end
+module Spree
+  LineItem.class_eval do
+    scope :assemblies, -> { joins(:product => :parts).uniq }
+  end
+end
