e05b3f6f4c291f7514de60fb92c76be13832c4c5
Daniel J. Pritchett <dpritchett@gmail.com>
2014-01-13 13:44:09 -0600

    Fixed framework regression on part controller

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -11,6 +11,13 @@ Spree::Product.class_eval do
     :foreign_key => "assembly_id"
 
   scope :individual_saled, where(["spree_products.individual_sale = ?", true])
+  
+  scope :search_can_be_part, -> (query) { not_deleted.available.joins(:master)
+     .where(arel_table["name"].matches("%#{query}%").or(Spree::Variant.arel_table["sku"].matches("%#{query}%")))
+     .where(can_be_part: true)
+     .limit(30)
+  }
+ 
 
   scope :active, lambda { |*args|
     not_deleted.individual_saled.available(nil, args.first)
